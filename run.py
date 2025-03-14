74697c4712149870e45ed8b327a81ba0b2339932
achristoforou1 <a_christoforou1@yahoo.com>
2025-03-14 18:49:56 +0200

    class board created and init function, print and self function

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Board:
+    """
+    Main board class. Sets board size, number of ships, player name and board tyoe (player or computer)
+    Has methods for adding ships and guesses and printing the board.
+    """
+def __init__(self, size, num_ships, name, type):
+    self.size = size
+    self.board = [("5" for x in range (size) for y in range (size))]
+    self.num_ships = num_ships
+    self.name = name
+    self.type = type
+    self.guesses = []
+    self.ships = []
+
+def print(self):
+    for row in self.board:
+        print("". join(row))
+
+def guess(self,x,y):
+    self.guesses.append(x,y)
+    self.board [x][y] = "x"
+
+    if (x,y) in self.ships:
+        self.board[x][y] = ""
+        return "Hit"
+    else:
+        return "Miss"
+    
